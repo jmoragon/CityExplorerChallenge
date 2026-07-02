fa4e6608201343d9701560e79494f8baeda0b058
jmoragon <jmoragon05@gmail.com>
2026-07-02 02:15:38 +0200

    Update AppDatabase.kt

--- app/src/main/java/com/example/cityexplorerfinal/local/AppDatabase.kt
+++ app/src/main/java/com/example/cityexplorerfinal/local/AppDatabase.kt
@@ -4,7 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.cityexplorerfinal.model.Challenge
 
-@Database(entities = [Challenge::class], version = 1, exportSchema = false)
+// Incrementamos la versión a 2 para forzar a Room a aplicar la migración destructiva
+@Database(entities = [Challenge::class], version = 7, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun challengeDao(): ChallengeDao
+
+    // Conectamos la base de datos con tu nuevo DAO
+    abstract fun challengeDao(): ChallengeDAO
+
 }
